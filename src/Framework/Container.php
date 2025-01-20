a5b0efdc4ddaca35f18f589435afd37368f163de
Jesse Emerson <jesseemerson7@gmail.com>
2025-01-19 21:22:48 -0500

    added new template engine logic for dependency injection

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Framework;
 
-use ReflectionClass;
+use ReflectionClass, ReflectionNamedType;
 use Framework\Exceptions\ContainerExceptions;
 
 class Container
@@ -22,6 +22,41 @@ class Container
         if (!$reflectionClass->isInstantiable()) {
             throw new ContainerExceptions("Class {$className} is non instantiable");
         }
-        dd($reflectionClass);
+
+        $constructor = $reflectionClass->getConstructor();
+
+        if (!$constructor) {
+            return $className;
+        }
+        $params = $constructor->getParameters();
+        if (count($params) === 0) {
+            return new $className;
+        }
+
+        $dependencies = [];
+        foreach ($params as $param) {
+            $name = $param->getName();
+            $type = $param->getType();
+            if (!$type) {
+                throw new ContainerExceptions("Failed to resolve class {$className} because param {$name} is missing a type hint.");
+            }
+            if (!$type instanceof ReflectionNamedType || $type->isBuiltin()) {
+                throw new ContainerExceptions("Failed to resolve class {$className} becuase invalid param name.");
+            }
+            $dependencies[] = $this->get($type->getName());
+        }
+
+        return $reflectionClass->newInstanceArgs($dependencies);
+    }
+    public function get(string $id)
+    {
+        if (!array_key_exists($id, $this->definitions)) {
+            throw new ContainerExceptions("class {$id} does not existin container");
+        }
+
+        $factory = $this->definitions[$id];
+        $depencency = $factory();
+
+        return $depencency;
     }
 }
